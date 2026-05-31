4386798ce9c4d406b9ded569a8e4730782f5bb72
MushiehEdison <mushiehedison66@gmal.com>
2026-05-31 15:06:38 +0100

    new

--- app.py
+++ app.py
@@ -1,3 +1,7 @@
+import sys
+import os
+sys.path.insert(0, os.path.join(os.path.dirname(__file__), 'safemum'))
+
 from SafeMumApp import create_app, db
 
 app = create_app()
@@ -6,4 +10,5 @@ with app.app_context():
     db.create_all()
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=False)
