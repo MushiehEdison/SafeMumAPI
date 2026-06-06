fdd9106fca268fe002e245d7ad594b42e35dbea2
MushiehEdison <mushiehedison66@gmal.com>
2026-06-06 02:55:23 +0100

    new

--- app.py
+++ app.py
@@ -1,21 +1,17 @@
 import sys
 import os
 
-# Add the directory containing app.py to the path
 sys.path.insert(0, os.path.dirname(os.path.abspath(__file__)))
 
-import dotenv
-dotenv.load_dotenv()
-
-print("DEBUG URI:", os.environ.get("SQLALCHEMY_DATABASE_URI", "NOT FOUND"))
+from dotenv import load_dotenv
+load_dotenv()
 
 from SafeMumApp import create_app, db
+from flask_migrate import Migrate
 
 app = create_app()
-
-with app.app_context():
-    db.create_all()
+migrate = Migrate(app, db)
 
 if __name__ == '__main__':
     port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port, debug=True)
+    app.run(host="0.0.0.0", port=port, debug=False)
